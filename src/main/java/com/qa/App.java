b87f0f5d6d526897552422b93a01cd73d3b52988
EC2 Default User <ec2-user@ip-172-31-47-237.eu-west-2.compute.internal>
2023-07-24 14:42:07 +0000

    Say hello to Issa

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -11,9 +11,10 @@ public class App
         System.out.println(sayHello());
         System.out.println(sayHelloToSomeone("Alice"));
         System.out.println(sayHelloToSomeone("Bob"));
-        System.out.println(sayHelloToSomeone("Charlie"));
+	System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
-        System.out.println(sayGoodbye());
+	System.out.println(sayHelloToSomeone("Issa"));
+	System.out.println(sayGoodbye());
     }
 
     public static String sayHello(){
